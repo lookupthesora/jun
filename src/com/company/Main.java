5ae5403e1460c93e9dffa6c7e93431b46bfcee8c
Jumpei Tada <jumpei.tada@necam.com>
2021-01-02 17:33:45 -0800

    initial commit for test

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.lang.String.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("Hello world");
+    }
+}
